361c48df1702491a9dfa35d2094143ef05fb548b
AliceHawk <122629469+AliceHawk@users.noreply.github.com>
2023-06-22 18:05:07 +0300

    Решение задачи 1 task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,17 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
+  let d = b**2 - 4 * a * c;
   let arr = [];
-  
+  if (d < 0) {
+  } else if (d == 0) {
+    let x = -b/(2*a);
+    arr.push(x);
+  } else if (d > 0) {
+    let x1 = (-b + Math.sqrt(d) )/(2*a);
+    let x2 = (-b - Math.sqrt(d) )/(2*a);
+    arr.push(x1);
+    arr.push(x2);
+  }
   return arr;
 }
 
