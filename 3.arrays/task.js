3f5b64a33747b0f293b6fbb03e602c3a5c3785f7
AliceHawk <122629469+AliceHawk@users.noreply.github.com>
2023-06-29 12:34:23 +0300

    новое решение первого задания

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,12 @@
-function compareArrays(arr1, arr2) {
-  let result = arr1.toString() === arr2.toString();
-  return result;
+ /* let result = arr1.toString() === arr2.toString();
+  return result;*/
+
+  function compareArrays(arr1, arr2) {
+   return arr1.every(function(elem, index){
+      return elem === arr2[index];
+   });
 }
+
 
 function getUsersNamesInAgeRange(users, gender) {
   
