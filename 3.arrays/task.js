1b84990a0537958cfcc4ad5a3412a8f2e8eff17d
AliceHawk <122629469+AliceHawk@users.noreply.github.com>
2023-06-29 12:40:20 +0300

    сравнение длинны массивов в первой задаче

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,12 +1,14 @@
  /* let result = arr1.toString() === arr2.toString();
   return result;*/
 
-  function compareArrays(arr1, arr2) {
+function compareArrays(arr1, arr2) {
+  if(arr1.length != arr2.length) {
+    return false;
+  }
    return arr1.every(function(elem, index){
       return elem === arr2[index];
    });
 }
-
 
 function getUsersNamesInAgeRange(users, gender) {
   
